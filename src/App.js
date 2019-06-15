f4d6b10d3f574e9f9b461c8c5ae1eb2f0e7cae65
Thomas Gardiner <thomgardiner@gmail.com>
2019-06-15 01:52:28 -0600

    more dynamic choice value array, randomization of characters

--- src/App.js
+++ src/App.js
@@ -2,77 +2,45 @@ import React, {useState, useContext, useReducer} from 'react';
 import OptionWindow from './component/OptionWindow'
 import OptionGrid from './component/OptionGrid'
 import StudyWindow from './component/StudyWindow'
-
 import hiraganaImport from './data/hiragana_new';
 import katakanaImport from './data/katakana_new';
 
 import './App.css';
 
-
-
-function appReducer(state, action){
-  switch (action.type){
-    case 'add': {
-      return 'something';
-    }
-    case 'something' : {
-      return 'something';
-    }
-    default:{
-      return state;
-    }
-  }
-}
-
-
 const App = props => {
   
   const [currentStep, setStep] = useState('start');
-  const [currentScore, setScore] = useState(0);
-  const [totalSeen, setSeen] = useState(0);
+  //const [currentScore, setScore] = useState(0);
+  //const [totalSeen, setSeen] = useState(0);
   const [dataSet, setDataSet] = useState('default');
-  const [totalQuestions, setTotal] = useState('0');
+ // const [totalQuestions, setTotal] = useState('0');
   const [studyObj, setStudyObj] = useState([]);
-  const [endlessMode, setEndless] = useState(false);
-  const [studyValues, setStudyValue] = useState([
-    true,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false
-])
-
+  const [studyValues, setStudyValue] = useState([])
 
 const setData = (data) => {
   if(data === 'hiragana'){
     setDataSet(hiraganaImport);
+    hiraganaImport.forEach(function(item){
+      studyValues.push(false);
+    })
+
+    let tempArr = studyValues.splice(0);
+    setStudyValue(tempArr);
   }
+
   else if(data === 'katakana'){
     setDataSet(katakanaImport);
+    katakanaImport.forEach(function(item){
+      studyValues.push(false);
+    })
+
+    let tempArr = studyValues.splice(0);
+    setStudyValue(tempArr);
   }
 }
 
 const setStudyValues = (data) => {
     setStudyValue(data);
-}
-
-const handleEndless = () => {
-    let val = !endlessMode;
-    setEndless(val);
-}
-
-const handleStudy = () => {
-  setStep('study');
 }
 
 const generateDeck = () => {
@@ -84,9 +52,8 @@ const generateDeck = () => {
     }
   })
   setStudyObj(tempObj);
-  handleStudy();
+  setStep('study');
 }
-
 
   return (
       <div className="App">
@@ -103,13 +70,12 @@ const generateDeck = () => {
         
         {/* STUDY */}
         {currentStep === 'study' ? 
-        <StudyWindow deck={studyObj.flat()} />: null}
+        <StudyWindow deck={studyObj.flat().sort(function(a, b){return 0.5 - Math.random()})} />: null}
         <div>
           </div>
       </div>
   )
   
-
 }
 
 export default App;
--- src/component/StudyWindow.js
+++ src/component/StudyWindow.js
@@ -6,10 +6,8 @@ const StudyWindow = props => {
     const [toSeePile, updateToSee] = useState(props.deck);
     //const [toSeePile, updateToSee] = useState(props.deck.sort(function(a, b){return 0.5 - Math.random()}));
     const [seenPile, updateSeen] = useState([]);
-    const [currentAnswer, setCurrentAnswer] = useState([]);
-    
-    let currentCharacter = toSeePile[0];
-    
+    const [currentAnswer, setCurrentAnswer] = useState();
+    const [currentCharacter, setCurrentCharacter] = useState(toSeePile[0]);
 
     const handleTextChange = (event) => {
         console.log(event.target.value);
@@ -17,27 +15,40 @@ const StudyWindow = props => {
     }
 
     const handleSubmit = () => {
-        if(currentAnswer === currentCharacter.romanization){
+
+        console.log(currentAnswer);
+
+        if(currentAnswer === undefined){
+            console.log('sup');
+            return;
+        }
+        else if(currentAnswer === currentCharacter.romanization){
             console.log("correct!");
             setCurrentAnswer('');
             seenPile.push(toSeePile[0]);
             toSeePile.shift();
+            setCurrentCharacter(toSeePile[0]);
         }
         else{
             console.log("false");
+            setCurrentAnswer('');
             toSeePile.push(toSeePile[0]);
             toSeePile.shift();
+            setCurrentCharacter(toSeePile[0]);
         }
     }
 
     return(
       <div className="study-window">
           <div className="characterDisplay">
-              {currentCharacter.character}
+              {currentCharacter ? currentCharacter.character : <div>Complete!</div>}
           </div>
           <div>
-            <input type="text" value={currentAnswer} onChange={handleTextChange}></input>
-            <button onClick={handleSubmit}>Submit</button>
+              {currentCharacter ? 
+                    <div>
+                        <input type="text" value={currentAnswer} onChange={handleTextChange}></input>
+                    <button onClick={handleSubmit}>Submit</button>
+                    </div> : null}
           </div>
       </div>
     )
