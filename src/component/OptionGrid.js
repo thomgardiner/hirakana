17b38730cb25f54a2b599651fdd4803e8cc1ce59
Thomas Gardiner <thomgardiner@gmail.com>
2019-06-14 21:06:38 -0600

    input select

--- src/component/OptionGrid.js
+++ src/component/OptionGrid.js
@@ -4,18 +4,23 @@ import './OptionGrid.css';
 
 const OptionGrid= props => {
 
+    const [studyObj, setStudyObj] = useState([]);
+    
+    const handleCheck = input => {
+        console.log(input + "is the input")
+    }
+
    return(
       <div className="options-grid">
-        
-        
-        
         {props.data.map((row, index) => (
-          
-          <div className="characterContainer" key={index}>
+        <div key={index + '_container'}className="container">
+        <input key={index + '-input'} type="checkbox" value="true" onChange={() => handleCheck(row)}></input>
+          <div className="characterContainer" key={index + '-char'}>
             {row.map((item, index1) => (
                     <div className="character-square" key={index + "_" + index1}> {row[index1].character}
                    </div>
             ))}
+        </div>
         </div>
         ))}
       </div>
